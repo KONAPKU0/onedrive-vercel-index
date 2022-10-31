35a486649ce384426788e4ef7fb0ac5386461eaf
youmu <KONPAKU@VIP.QQ.COM>
2022-10-31 11:16:48 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/电子邮件附件','/主储存','/临时储存','/个人','/Office Online 扩展','/Microsoft Edge Collections','/Documents'],
+  protectedRoutes: ['/'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:spencer.wushangbo@gmail.com',
